c432fe048c71f959d95dde2c78b6aabb0477d178
jai <jai.bhalla2021@vitstudent.ac.in>
2023-05-10 21:32:43 +0530

    Adding the mobile view

--- components/Features.js
+++ components/Features.js
@@ -3,8 +3,8 @@ import Feature from "@/components/Feature";
 
 const Features = () => {
     return (
-        <div className={'sm:px-16 md:px-24 lg:px-28 sm:py-4 md:py-20'}>
-            <div className={'flex sm:flex-col md:flex-row justify-between'}>
+        <div className={'px-4 pt-4 sm:px-16 md:px-24 lg:px-28 sm:py-4 md:py-20'}>
+            <div className={'flex flex-col sm:flex-col md:flex-row justify-between'}>
                 <Feature heading={'Discover amazing artworks!'} description={'We have more than 5000 artworks made by myriads of artists'}/>
                 <Feature heading={'Discover your type!'} description={'We have more than 500 artists working with us. Discover what type artists inspires you.'}/>
                 <Feature heading={'Invest in Artworks!'} description={'Buy, track and sell art by only a few clicks only at ARTICUE.'}/>
--- components/Hero.js
+++ components/Hero.js
@@ -22,39 +22,44 @@ const Hero = () => {
         setCurrentSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1);
     };
     return (
-        <div className={'sm:h-[200vh] md:h-[100vh] sm:px-16 md:px-24 flex sm:flex-col md:flex-row lg: px-28 bg-gray-950'}>
-            <div className={'flex flex-col gap-14'}>
-                <div className={'pt-20'}>
-                    <div className={'flex'}>
-                        <h1 className={'text-5xl font-light mt-4 flex'}>
+        <div className={'flex flex-col px-4 md:h-[100vh] sm:px-16 md:px-24 md:flex-row lg:px-28 bg-gray-950'}>
+            <div className={'flex flex-col gap-5 sm:gap-14'}>
+                <div className={'pt-6 sm:pt-20'}>
+                    <div className={'flex flex-col sm:flex-row'}>
+                        <h1 className={'text-5xl font-light sm:mt-5 flex'}>
                             Welcome to</h1>
-                        <Image className={'mx-4'} src={logo} alt={''} width={230} height={230}/>
+                        <Image
+                            src={logo}
+                            alt=""
+                            className="ml-32 sm:ml-0 w-52 h-20 sm:w-64 sm:h-24"
+                        />
+
                     </div>
-                    <h1 className={'text-xl'}>
+                    <h1 className={'text-lg sm:text-xl'}>
                         the premier online marketplace for art enthusiasts seeking exceptional and affordable works of art.
                     </h1>
                 </div>
                 <div>
                     <button className={'text-white p-4 text-xl transition-all duration-500 text-white bg-gray-950 shadow-sm shadow-gray-800 rounded-md hover:scale-110 active:scale-90'}>Get Started!</button>
                 </div>
-                <ul className={'flex justify-between p-2 shadow-sm shadow-gray-800 rounded-sm'}>
-                    <li className={'flex'}>
-                        <h1 className={'text-5xl'}>10</h1>
-                        <p className={'ml-2 text-gray-500 w-[100px]'}>Our active Artists</p>
+                <ul className={'flex flex-col sm:flex-row justify-between p-2 shadow-sm shadow-gray-800 rounded-sm'}>
+                    <li className={'flex justify-around border-b-[1px] border-gray-900 sm:border-none'}>
+                        <h1 className={'text-4xl sm:text-5xl'}>10</h1>
+                        <p className={'ml-2 text-sm sm:text-md items-center flex flex-col justify-center text-gray-500 w-[100px]'}>Our active Artists</p>
                     </li>
-                    <li className={'flex'}>
-                        <h1 className={'text-5xl'}>20</h1>
-                        <p className={'ml-2 text-gray-500 w-[100px]'}>These many Artworks</p>
+                    <li className={'flex justify-around border-b-[1px] border-gray-900 sm:border-none'}>
+                        <h1 className={'text-4xl sm:text-5xl'}>20</h1>
+                        <p className={'ml-2 text-sm sm:text-md items-center flex flex-col justify-center text-gray-500 w-[100px]'}>These many Artworks</p>
                     </li>
-                    <li className={'flex'}>
-                        <h1 className={'text-5xl'}>13</h1>
-                        <p className={'ml-2 text-gray-500 w-[100px]'}>Artworks sold</p>
+                    <li className={'flex justify-around border-b-[1px] border-gray-900 sm:border-none'}>
+                        <h1 className={'text-4xl sm:text-5xl'}>13</h1>
+                        <p className={'ml-2 text-sm sm:text-md items-center flex flex-col justify-center text-gray-500 w-[100px]'}>Artworks sold</p>
                     </li>
                 </ul>
             </div>
-            <div className="h-[100vh] relative mt-5 shadow-sm shadow-gray-800 md:ml-5 rounded-sm w-full">
+            <div className="h-[45vh] sm:h-[90vh] relative mt-5 shadow-sm shadow-gray-800 md:ml-5 rounded-sm w-full">
                 <div className="absolute inset-0 flex items-center justify-center overflow-hidden">
-                    <div className="w-[80%] sm:h-[80vh] h-[60vh] md:h-[60vh] lg:h-[70vh] xl:h-[80vh] overflow-hidden rounded-md">
+                    <div className="w-[80%] h-[40vh] sm:h-[80vh] md:h-[60vh] lg:h-[70vh] xl:h-[80vh] overflow-hidden rounded-md">
                         <Image
                             src={slides[currentSlide]}
                             alt="Artwork"
@@ -62,18 +67,18 @@ const Hero = () => {
                         />
                     </div>
                 </div>
-                <div className="absolute z-10 right-6 top-1/2 transform -translate-y-1/2">
+                <div className="absolute z-10 right-4 sm:right-6 top-1/2 transform -translate-y-1/2">
                     <button
                         onClick={prevSlide}
-                        className="text-white p-4 text-xl transition-all duration-500 text-white bg-gray-950 shadow-sm shadow-gray-800 rounded-md hover:scale-110 active:scale-90"
+                        className="text-white p-2 sm:p-4 text-xl transition-all duration-500 text-white bg-gray-950 shadow-sm shadow-gray-800 rounded-md hover:scale-110 active:scale-90"
                     >
                         <AiOutlineArrowRight/>
                     </button>
                 </div>
-                <div className="absolute z-10 left-6 top-1/2 transform -translate-y-1/2">
+                <div className="absolute z-10 left-4 sm:left-6 top-1/2 transform -translate-y-1/2">
                     <button
                         onClick={nextSlide}
-                        className="text-white p-4 text-xl transition-all duration-500 text-white bg-gray-950 shadow-sm shadow-gray-800 rounded-md hover:scale-110 active:scale-90"
+                        className="text-white p-2 sm:p-4 text-xl transition-all duration-500 text-white bg-gray-950 shadow-sm shadow-gray-800 rounded-md hover:scale-110 active:scale-90"
                     >
                         <AiOutlineArrowLeft/>
                     </button>
