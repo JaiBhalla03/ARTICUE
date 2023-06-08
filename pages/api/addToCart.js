1f6e9494a0fc253f1ca9ee91a645902af2feb563
jai <jai.bhalla2021@vitstudent.ac.in>
2023-06-08 12:28:13 +0530

    OK so the api is working

--- pages/api/addToCart.js
+++ pages/api/addToCart.js
@@ -1,31 +1,30 @@
 import prisma from '../../prisma/lib/client';
 
-export default async function handler(req, res){
-    try{
-        const {userId, artworkId} = req.body;
-        //retrieving the user and artworks of the basis of the user and artworks provided
-        console.log({userId, artworkId})
+export default async function handler(req, res) {
+    try {
+        const { userId, artworkId } = req.body;
+
+        // Retrieve the user and artwork based on the provided IDs
         const user = await prisma.user.findUnique({
-            where : {id: userId},
-        })
-        const artwork = await prisma.artwork.findUnique({
-            where: {id: artworkId}
-        })
-        if(!user || !artwork) {
-            return res.status(404).json({error: 'User or artwork not found'})
+            where: { id: userId },
+        });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User or artwork not found' });
         }
-        //creating the cart item and associating with the user and artworks
-        console.log('we can proceed')
+
+        // Create the cart item and associate it with the user and artwork
+        console.log('We can proceed');
         const cartItem = await prisma.cartItem.create({
-            data:{
-                userId: userId,
-                artworkId: artworkId
+            data: {
+                userId: user.id,
+                artworkId
             },
-        })
-        return res.status(200).json({message: 'cartItem created', cartItem});
-    }
-    catch(err){
-        console.error('error adding the cartItem');
-        res.status(500).json({error: 'An error occurred while adding the cart items'});
+        });
+
+        return res.status(200).json({ message: 'CartItem created', cartItem });
+    } catch (err) {
+        console.error('Error adding the cartItem:', err);
+        return res.status(500).json({ error: 'An error occurred while adding the cart items' });
     }
 }
